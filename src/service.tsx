ec0fa2b1de18bb2fd05c2a58f0eaa024637e5db5
Strato Ivan Di Luccio <ivan.diluccio@unitiva.it>
2021-05-28 18:24:16 +0200

    feat: added mockup and api endpoint

---
+++ src/service.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export interface Product {
+  UPC: number
+  name: string
+  price: {
+    current: {
+      value: number
+    }
+  }
+  aviability: {
+    stock: number
+  }
+  variants: Variant[]
+}
+
+interface Variant {
+  UPC: number
+  name: string
+  price: {
+    current: {
+      value: number
+    }
+  }
+}
+
+export const getProducts = () => axios.get<Product[]>('htp://fe-test.unitiva.it/products/products.json')
+
